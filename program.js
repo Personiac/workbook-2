68f67f214744ba89b49ebeaee87dc8b35405b7ac
Jonathan Tran <trandingtopics@gmail.com>
2023-04-25 10:57:58 -0400

    Added .toFixed(2)

--- program.js
+++ program.js
@@ -32,7 +32,7 @@ var tip = 12.00;
 var totalDue = foodCost + tax + tip;
 
 console.log("Food cost is " + foodCost + " and tax is " + tax);
-console.log("Tip is " + tip);
+console.log("Tip is " + tip.toFixed(2));
 console.log("Total Due is " + totalDue);
    
 
