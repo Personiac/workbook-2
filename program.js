7d452940beec3fc36c4313413d1dc0aa69e287ec
Jonathan Tran <trandingtopics@gmail.com>
2023-04-25 10:51:48 -0400

    Retyped totalDue

--- program.js
+++ program.js
@@ -29,7 +29,8 @@ var foodCost = 79.25;
 var tax = 6.54;
 var tip = 12.00;
 
-var totalDue = foodCost + tax + tip;
-
-console.log("The total due is " + totalDue);
+console.log("Food cost is " + foodCost + " and tax is " + tax);
+console.log("Tip is " + tip);
+console.log("Total Due is " + totalDue);
+   
 
