d08c35eba20d610a1805df54a42de9f65847af18
Jonathan Tran <trandingtopics@gmail.com>
2023-04-25 14:04:33 -0400

    practice

--- net-worth.js
+++ net-worth.js
@@ -3,10 +3,11 @@
 
 // var netWorth = assets - debts;
 
-var obj = { assets: 2000, debts: 1500 } //declare object ={declare variables}
+var obj = { assets: 2000, debts: 1500 }; //declare object ={declare variables}
 // var Math {PI: 3.14157}
 var netWorth = obj.assets - obj.debts;
 
 console.log("Your net worth is " + netWorth);
 console.log(Math.PI);
 console.log(Math.round(Math.PI));
+console.log(Math.PI.toFixed(2));
