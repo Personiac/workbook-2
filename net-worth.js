b7f0e8b5b28b479bea856c6c3c551e64d3c0e910
Jonathan Tran <trandingtopics@gmail.com>
2023-04-25 13:21:44 -0400

    object and function

--- net-worth.js
+++ net-worth.js
@@ -1,5 +1,12 @@
-var assets = 2000;
-var debts = 1500;
+// var assets = 2000;
+// var debts = 1500;
 
-var netWorth = assets - debts;
+// var netWorth = assets - debts;
+
+var obj = { assets: 2000, debts: 1500 } //declare object ={declare variables}
+// var Math {PI: 3.14157}
+var netWorth = obj.assets - obj.debts;
+
 console.log("Your net worth is " + netWorth);
+console.log(Math.PI);
+console.log(Math.round(Math.PI));
